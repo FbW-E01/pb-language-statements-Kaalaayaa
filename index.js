7ddd0fcc66750492bc87fd63203b2dbb40fd1fd3
Kaalaayaa <kalaya.chatelain@yahoo.fr>
2021-03-30 10:22:10 +0200

    done

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log("Hello world!"
+
+// console.log("this is my first js file.")
+
+
+/*response : the statement doesnt print and this error appears = console.log("Hello world!"
+            ^^^^^^^^^^^^^^
+
+SyntaxError: missing ) after argument list*/
